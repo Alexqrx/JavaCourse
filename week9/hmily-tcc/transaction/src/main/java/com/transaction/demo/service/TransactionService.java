697fbc0cd4d058a8532de696ea44b8c2a211918a
Alexqrx <869645020@qq.com>
2021-11-21 23:30:20 +0800

    第九周作业

---
+++ week9/hmily-tcc/transaction/src/main/java/com/transaction/demo/service/TransactionService.java
@@ -0,0 +1,5 @@
+package com.transaction.demo.service;
+
+public interface TransactionService {
+    void transaction();
+}
---
+++ week9/hmily-tcc/transaction/src/test/java/com/transaction/demo/TransactionServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.transaction.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TransactionServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
